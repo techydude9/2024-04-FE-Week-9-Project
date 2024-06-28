e79121fc58cc7186b04a367d99fc8ef1d3734f20
techydude9 <bob.ruzga@gmail.com>
2024-06-28 13:53:21 -0400

    Full Game code but not Working

--- wk9.tests.js
+++ wk9.tests.js
@@ -2,7 +2,11 @@
     Author: Bob Ruzga
     Date: June 24, 2024
     Testing js file
-*/
+
+    Installed an older version of Mocha and Chai
+    see package.json
+    */
 
 const expect = chai.expect
 const assert = chai.assert
+
